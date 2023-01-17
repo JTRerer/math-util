d3a0be2068474d2507275e33cabcc04d3f66606b
JTRerer <trongan147895@gmail.com>
2023-01-17 18:48:25 +0700

    Second Commit-README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# WELCOME TO MY FIRST REPOSITORY - MATHUTIL PROJECT
+
+## You will find in this repo:
+
+* NetBeans Apache Ant project 
+* .gitignore file for NetBeans project
+* README.md - this file itself - using Markdown language
+
+### 2023 JTRer | jtrer147@gmail.com
--- src/com/fu/mathutil/MathUtilMain.java
+++ src/com/fu/mathutil/MathUtilMain.java
@@ -16,10 +16,16 @@ public class MathUtilMain {
         long result = MathUtil.getFactorial(5); //ham tinh di coi la may
 
         System.out.println("expected: 5! = 120; actual: " + result);
+
         System.out.println("expected: 6! = 720; actual: " + MathUtil.getFactorial(6));
         System.out.println("expected: 0! = 1; actual: " + MathUtil.getFactorial(0));
 
         //MathUtil.getFactorial(-5); nem ngoai le that roi
+        
+        //THEM CODE SAU FIRST COMMIT - CODE LEN SERVER 6:36PM 1/17/2023
+        System.out.println("expected: 1! = 1; actual: " + MathUtil.getFactorial(1));
+        System.out.println("expected: 3! = 6; actual: " + MathUtil.getFactorial(3));
+
         //ky thuat kiem thu phan mem: uoc luong gia tri tra ve cua ham la gi
         //expected value: 5! = 120
         //may/ham ai do viet khi chay thuc te la may - actual, gia su 120
